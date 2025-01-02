a990d63bbef3269aca82100163cb9938b1d0c250
Mario Campbell <contacto@mariocampbell.com.ar>
2025-01-02 01:47:49 -0300

    feat: add config rgb enable

---
+++ config.h
@@ -0,0 +1,14 @@
+// Habilitar soporte para iluminación RGB
+#define RGBLIGHT_ANIMATIONS  // Activa todas las animaciones predefinidas
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_LIMIT_VAL 120  // Ajusta el brillo máximo (0-255)
+#define RGBLIGHT_SLEEP       // Apaga los LEDs al entrar en reposo
+#define RGBLIGHT_HUE_STEP 8  // Ajusta la velocidad de cambio de tono
+#define RGBLIGHT_SAT_STEP 8  // Ajusta la velocidad de cambio de saturación
+#define RGBLIGHT_VAL_STEP 8  // Ajusta la velocidad de cambio de brillo
